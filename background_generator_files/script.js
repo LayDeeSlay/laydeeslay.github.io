5c3d025cbdeb349de7ca8cebe3caa58b012ef5f0
Jane <janehortonthiem@gmail.com>
2018-09-07 15:33:16 -0600

    Section 11, Lecture 110
    Background generator improvements.

--- background_generator_files/script.js
+++ background_generator_files/script.js
@@ -2,6 +2,8 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomButton = document.querySelector(".randomButton")
+setGradient();
 
 function setGradient() {
 	body.style.background = 
@@ -14,6 +16,14 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function setRandomGradient() {
+	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+randomButton.addEventListener("click", setRandomGradient);
